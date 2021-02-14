8037b09d2a5b5ad67479597a33af37f22e70c8e3
havinhphu <havinhphu188@gmail.com>
2021-02-14 17:59:46 +0700

    Integrate PrimeNG

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import { WallComponent } from './wall/wall.component';
+import {InputTextModule} from 'primeng/inputtext';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { WallComponent } from './wall/wall.component';
   imports: [
     BrowserModule,
     HttpClientModule,
+    InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/wall/wall.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+@Component({
+  selector: 'app-wall',
+  templateUrl: './wall.component.html',
+  styleUrls: ['./wall.component.css']
+})
+export class WallComponent implements OnInit {
+  articles: IArticle[];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.getArticles().subscribe((data: IArticle[]) => this.articles = data);
+  }
+
+  getArticles(): Observable<IArticle[]>{
+    return this.http.get<IArticle[]>('api/article/all');
+  }
+
+}
+
+interface IArticle{
+  id: number;
+  title: string;
+  content: string;
+}
