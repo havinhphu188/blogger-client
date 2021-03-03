1229eba2b923cd983f9c5d0e76824ec45c6f9a87
havinhphu <havinhphu188@gmail.com>
2021-03-03 10:38:06 +0700

    re navigate to login page

---
+++ src/app/guard/auth.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router} from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private router: Router) {
+  }
+
+  isLogin(): boolean {
+    this.router.navigate(['/login']);
+    return false;
+  }
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree
+  {
+    return this.isLogin();
+  }
+}
--- src/app/module/app-routing.module.ts
+++ src/app/module/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {WallComponent} from '../component/wall/wall.component';
 import {LoginComponent} from '../component/login/login.component';
+import {AuthGuard} from '../guard/auth.guard';
 
 
 const routes: Routes = [
-  { path: 'wall', component: WallComponent },
+  { path: 'wall', component: WallComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent }
 ];
 
