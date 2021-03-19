b2e163370dc3c390e2a7f45f91e9c8bea743e1f0
havinhphu <havinhphu188@gmail.com>
2021-03-19 18:30:24 +0700

    logout, navigate to login page when token expire

--- src/app/interceptor/general-interceptor.ts
+++ src/app/interceptor/general-interceptor.ts
@@ -8,6 +8,8 @@ import {catchError, finalize} from 'rxjs/operators';
 import {Observable, throwError} from 'rxjs';
 import {LoadingService} from '../service/common-service/loading.service';
 import {MessageService} from 'primeng/api';
+import {Router} from '@angular/router';
+import {AuthService} from '../service/common-service/auth.service';
 
 // Show spinner loading, toast error on requestFail.
 @Injectable()
@@ -15,7 +17,9 @@ export class GeneralInterceptor implements HttpInterceptor {
   private totalRequests = 0;
 
   constructor(private loadingService: LoadingService,
-              private messageService: MessageService) { }
+              private messageService: MessageService,
+              private router: Router,
+              private authService: AuthService) { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     this.totalRequests++;
@@ -23,7 +27,12 @@ export class GeneralInterceptor implements HttpInterceptor {
 
     return next.handle(request).pipe(
       catchError((error: HttpErrorResponse) => {
-        this.messageService.add({severity: 'error', summary: 'Error Status: ' + error.status, detail: error.error.message});
+        const errorMsg = (error.error != null ? error.error.message : error.statusText);
+        this.messageService.add({severity: 'error', summary: 'Error Status: ' + error.status, detail: errorMsg});
+        if (error.status === 401){
+          this.authService.logout();
+          this.router.navigate(['login']);
+        }
         return throwError(error);
       }),
       finalize(() => {
