7f79bb712b1fbc50802b9f185134c64d4a89d450
havinhphu <havinhphu188@gmail.com>
2021-04-13 00:19:53 +0700

    - Organize Dashboard for authenticated and non authenticated user - Show text when subscription is empty

--- src/app/component/dashboard/dashboard.component.ts
+++ src/app/component/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from '../../service/common-service/auth.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  isLogin: boolean;
 
-  constructor() { }
+  constructor(private authService: AuthService) {
+  }
 
   ngOnInit(): void {
+    this.isLogin = this.authService.getIsLogin();
   }
 
 }
